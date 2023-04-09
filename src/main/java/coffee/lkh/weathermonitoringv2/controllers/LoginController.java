e57546a7a2e732da9aaa71f90664bc22ccbf6f5d
nakira974 <maxime.loukhal@gmail.com>
2023-04-10 01:06:27 +0200

    Update LoginController.java and SecurityConfig.java

--- src/main/java/coffee/lkh/weathermonitoringv2/controllers/LoginController.java
+++ src/main/java/coffee/lkh/weathermonitoringv2/controllers/LoginController.java
@@ -3,7 +3,16 @@ package coffee.lkh.weathermonitoringv2.controllers;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.server.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -25,7 +34,15 @@ public class LoginController {
     private String issuerUri;
 
     @RequestMapping("/login")
-    public String login(HttpServletRequest request, Model model) {
+    public String login(HttpServletRequest request, Model model,
+                        @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
+                        OAuth2AuthenticationToken authentication) {
+
+        if (authentication != null && authentication.isAuthenticated()) {
+            // Redirect to home page if user is already authenticated
+            return "redirect:/";
+        }
+
         // Create a new CSRF token
         CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
 
@@ -34,15 +51,23 @@ public class LoginController {
             model.addAttribute("_csrf", csrfToken);
         }
 
-        // Construct the URL for the Keycloak login page
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(issuerUri)
-                .path("/auth/realms/weathermonitoring/protocol/openid-connect/auth")
-                .queryParam("response_type", "code")
-                .queryParam("client_id", clientId)
-                .queryParam("redirect_uri", getRedirectUri(request))
-                .queryParam("scope", "openid");
+        if (authorizedClient == null) {
+            // Construct the URL for the Keycloak login page
+            UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(issuerUri)
+                    .path("/auth/realms/weathermonitoring/protocol/openid-connect/auth")
+                    .queryParam("response_type", "code")
+                    .queryParam("client_id", clientId)
+                    .queryParam("redirect_uri", getRedirectUri(request))
+                    .queryParam("scope", "openid");
 
-        return "redirect:" + builder.buildAndExpand("weathermonitoring").toUriString();
+            return "redirect:" + builder.buildAndExpand("weathermonitoring").toUriString();
+        } else {
+            // Save the authorized client details for the authenticated user
+            authorizedClientService.saveAuthorizedClient(authorizedClient, authentication);
+
+            // Redirect to the home page for authenticated users
+            return "redirect:/";
+        }
     }
 
     private String getRedirectUri(HttpServletRequest request) {
