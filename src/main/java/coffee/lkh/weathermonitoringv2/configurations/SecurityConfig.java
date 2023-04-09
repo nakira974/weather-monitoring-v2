f9ed6d0b44d565adfee57f3c58af673910e2cde0
nakira974 <maxime.loukhal@gmail.com>
2023-04-09 12:48:36 +0200

    Fixed login and security configurations for csrfToken

--- src/main/java/coffee/lkh/weathermonitoringv2/configurations/SecurityConfig.java
+++ src/main/java/coffee/lkh/weathermonitoringv2/configurations/SecurityConfig.java
@@ -1,5 +1,7 @@
 package coffee.lkh.weathermonitoringv2.configurations;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,26 +11,32 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
 @Import(OAuth2Config.class)
-class SecurityConfig {
+public class SecurityConfig  {
+
 
     @Autowired
     private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService;
 
-    private final KeycloakLogoutHandler keycloakLogoutHandler;
+    @Autowired
+    private LogoutHandler keycloakLogoutHandler;
 
-    SecurityConfig(KeycloakLogoutHandler keycloakLogoutHandler) {
-        this.keycloakLogoutHandler = keycloakLogoutHandler;
-    }
 
     @Bean
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
@@ -36,36 +44,48 @@ class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        /*
-        http.authorizeRequests()
-                .requestMatchers("/weather*", "/swagger-ui*")
-                .hasRole("user")
-                .anyRequest()
-                .permitAll();*/
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 
-        http.authorizeHttpRequests() .requestMatchers("/login**", "/error**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        // Create some dummy users for testing purposes
+        var user = org.springframework.security.core.userdetails.User
+                .withUsername("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        var admin = org.springframework.security.core.userdetails.User
+                .withUsername("admin")
+                .password("password")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(List.of(user, admin));
+    }
+    @Bean
+    public DefaultOAuth2UserService defaultOAuth2UserService() {
+        return new DefaultOAuth2UserService();
+    }
+
+    @Bean SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+
+        http.authorizeRequests()
+                .requestMatchers("/login", "/error").permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .oauth2Login()
                 .userInfoEndpoint()
                 .oidcUserService(oidcUserService)
-                .and()
-                .loginPage("/login")
-                .and()
-                .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/");
+                .and().defaultSuccessUrl("/weather?city=Abbeville&country=US&state=LA").permitAll()
+                .and().logout().addLogoutHandler(keycloakLogoutHandler)
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID");
 
-
-        http.oauth2Login()
-                .and()
-                .logout()
-                .addLogoutHandler(keycloakLogoutHandler)
-                .logoutSuccessUrl("/");
         http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+
         return http.build();
     }
 }
--- src/main/java/coffee/lkh/weathermonitoringv2/controllers/LoginController.java
+++ src/main/java/coffee/lkh/weathermonitoringv2/controllers/LoginController.java
@@ -1,30 +1,53 @@
 package coffee.lkh.weathermonitoringv2.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.web.server.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Controller
 public class LoginController {
 
-    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+    @Autowired
+    private OAuth2AuthorizedClientService authorizedClientService;
 
-    @GetMapping("/login")
-    public String login(HttpServletRequest request, @AuthenticationPrincipal OidcUser user) {
-        if (user != null) {
-            logger.info("User already logged in: {}", user.getEmail());
-            return "redirect:/weather?city=Abbeville&country=US&state=LA";
-        } else {
-            OAuth2AuthenticationToken token =
-                    (OAuth2AuthenticationToken) request.getUserPrincipal();
-            logger.info("User is logging in: {}", token.getPrincipal().getAttributes().get("email"));
-            return "redirect:/login/oauth2/authorization/keycloak";
+    @Value("${spring.security.oauth2.client.registration.keycloak.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.provider.keycloak.issuer-uri}")
+    private String issuerUri;
+
+    @RequestMapping("/login")
+    public String login(HttpServletRequest request, Model model) {
+        // Create a new CSRF token
+        CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+
+        // Pass the token to the view
+        if (csrfToken != null) {
+            model.addAttribute("_csrf", csrfToken);
         }
+
+        // Construct the URL for the Keycloak login page
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(issuerUri)
+                .path("/auth/realms/weathermonitoring/protocol/openid-connect/auth")
+                .queryParam("response_type", "code")
+                .queryParam("client_id", clientId)
+                .queryParam("redirect_uri", getRedirectUri(request))
+                .queryParam("scope", "openid");
+
+        return "redirect:" + builder.buildAndExpand("weathermonitoring").toUriString();
     }
 
+    private String getRedirectUri(HttpServletRequest request) {
+        ServletUriComponentsBuilder builder = ServletUriComponentsBuilder.fromRequest(request);
+        builder.replacePath("/login/oauth2/code/custom");
+        return builder.toUriString();
+    }
 }
